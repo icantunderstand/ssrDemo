fc34c0b065c911319c516dadb0cdc03ccb826ca1
sunhao14 <sunhao14@meituan.com>
2019-04-12 10:16:01 +0800

    fix:

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,10 @@ module.exports = {
       'no-console': 'off',
       'react/prop-types': 'off',
       'react/prefer-stateless-function': 'off',
-      'react/jsx-filename-extension' : 'off'
+      'react/jsx-filename-extension' : 'off',
+      'import/no-extraneous-dependencies' : 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'class-methods-use-this': 'off'
     }
 };
